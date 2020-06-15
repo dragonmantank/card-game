0736eeedb971813d5d81c5e4962e97aa9dc41689
xliu-vonage <63656264+xliu-vonage@users.noreply.github.com>
2020-06-15 16:30:48 -0400

    cardtable

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,6 @@
 // replace these values with those generated in your TokBox Account
 console.log('app.js started')
 // (optional) add server code here
-var apiKey = '45828062';
-var sessionId = '2_MX40NTgyODA2Mn5-MTU5MjI0NjgxMjA2Nn5RS3l4OUpSNE5wb3k0aUJPLzFvM1loTjB-UH4';
-var token = 'T1==cGFydG5lcl9pZD00NTgyODA2MiZzaWc9ZWE2ZjUxMGE0ZGVhMTE4NDI0ZWViNDgzYWNmMjRlNzQ3ZTJhODU0YzpzZXNzaW9uX2lkPTJfTVg0ME5UZ3lPREEyTW41LU1UVTVNakkwTmpneE1qQTJObjVSUzNsNE9VcFNORTV3YjNrMGFVSlBMekZ2TTFsb1RqQi1VSDQmY3JlYXRlX3RpbWU9MTU5MjI0NjgzMyZub25jZT0wLjMzODU2MTMwNDE3MjczOTMzJnJvbGU9cHVibGlzaGVyJmV4cGlyZV90aW1lPTE1OTIzMzMyMzM=';
-/*
 var SERVER_BASE_URL = 'https://cardgamev.herokuapp.com';
 fetch(SERVER_BASE_URL + '/session').then(function(res) {
    return res.json()
@@ -14,7 +10,6 @@ fetch(SERVER_BASE_URL + '/session').then(function(res) {
    token = res.token;
    initializeSession();
 }).catch(handleError);
-*/
 // (optional) add server code here
 initializeSession();
 
